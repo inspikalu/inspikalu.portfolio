c0706790fd10b16349cfb0e2cd8d4c3fed409686
Clement Kalu Okereke <inspikalu@gmail.com>
2025-01-03 21:46:43 +0100

    Added contact sectoin

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "@/components/sections/Skills";
 import Experience from "@/components/sections/Experience";
 import Projects from "@/components/sections/Projects";
 import Line from "@/components/ui/Line";
+import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Line />
       <Projects />
       <Line />
+      <Contact />
     </div>
   );
 }
